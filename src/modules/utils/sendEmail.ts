89eb7c9b1e205f44a477e96832671e72235343c4
jcamposforner <jcamposforner@gmail.com>
2019-10-15 20:44:42 +0200

    Mensaje de confirmacion email

--- src/modules/utils/sendEmail.ts
+++ src/modules/utils/sendEmail.ts
@@ -1,26 +1,26 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
 export async function sendEmail(email: string, url: string) {
   const account = {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
-  console.log(account)
+  };
+
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: account.user,
       pass: account.pass
     }
-  })
+  });
 
   const mailOptions = {
     from: '"Confirmación email" <jesus.alu.ua@gmail.com>',
     to: email,
-    subject: 'Confirmación email',
-    text: 'Confirmación email',
-    html: `<a href="${url}">${url}</a>`
-  }
+    subject: "Confirmación email",
+    text: "Confirmación email",
+    html: `<a href="${url}">Confirmar email</a>`
+  };
 
-  await transporter.sendMail(mailOptions)
+  await transporter.sendMail(mailOptions);
 }
